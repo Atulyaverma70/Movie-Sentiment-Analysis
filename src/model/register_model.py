96df87e24fb03d1294397cc6164835f2e93720db
Atulya Verma <atulyaverma7068@gmail.com>
2026-05-15 02:50:32 +0530

    flask_app

---
+++ src/model/register_model.py
@@ -0,0 +1,94 @@
+# register model
+
+import json
+import mlflow
+import logging
+from src.logger import logging
+import os
+import dagshub
+
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+warnings.filterwarnings("ignore")
+
+# Below code block is for production use
+# -------------------------------------------------------------------------------------
+# Set up DagsHub credentials for MLflow tracking
+# dagshub_token = os.getenv("CAPSTONE_TEST")
+# if not dagshub_token:
+#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+
+# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+# dagshub_url = "https://dagshub.com"
+# repo_owner = "vikashdas770"
+# repo_name = "YT-Capstone-Project"
+# # Set up MLflow tracking URI
+# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+# -------------------------------------------------------------------------------------
+
+
+# Below code block is for local use
+# -------------------------------------------------------------------------------------
+mlflow.set_tracking_uri('https://dagshub.com/Atulyaverma70/Capstone_project.mlflow')
+dagshub.init(repo_owner='Atulyaverma70', repo_name='Capstone_project', mlflow=True)
+# -------------------------------------------------------------------------------------
+
+
+def load_model_info(file_path: str) -> dict:
+    """Load the model info from a JSON file."""
+    try:
+        with open(file_path, 'r') as file:
+            model_info = json.load(file)
+        logging.debug('Model info loaded from %s', file_path)
+        return model_info
+    except FileNotFoundError:
+        logging.error('File not found: %s', file_path)
+        raise
+    except Exception as e:
+        logging.error('Unexpected error occurred while loading the model info: %s', e)
+        raise
+
+def register_model(model_name: str, model_info: dict):
+    """Register the model to MLflow Model Registry."""
+    try:
+        client = mlflow.tracking.MlflowClient()
+
+        model_uri = f"runs:/{model_info['run_id']}/{model_info['model_path']}"
+
+        # create new model version
+        model_version = client.create_model_version(
+            name=model_name,
+            source=model_uri,
+            run_id=model_info['run_id']
+        )
+
+        # move to staging
+        client.transition_model_version_stage(
+            name=model_name,
+            version=model_version.version,
+            stage="Staging"
+        )
+
+        logging.info(
+            f"Model {model_name} version {model_version.version} registered successfully."
+        )
+
+    except Exception as e:
+        logging.error("Error during model registration: %s", e)
+        raise
+
+def main():
+    try:
+        model_info_path = 'reports/experiment_info.json'
+        model_info = load_model_info(model_info_path)
+        
+        model_name = "my_model"
+        register_model(model_name, model_info)
+    except Exception as e:
+        logging.error('Failed to complete the model registration process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
